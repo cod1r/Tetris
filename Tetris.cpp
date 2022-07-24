a7676a5851761f167ec67ab77cdbc5c6cc91fef6
cod1r <jasonho353@gmail.com>
2022-07-23 23:16:52 -0500

    added level speed for falling tetromino by using chrono, switched to SDL_Renderer for hardware acceleration, also keep running into stack-smashing and  error

--- Tetris.cpp
+++ Tetris.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <SDL2/SDL.h>
 #include <stdexcept>
-#include <ctime>
+#include <chrono>
 #include "Tetris.h"
 
 Tetris::Tetris(int width, int height)
@@ -10,104 +10,114 @@ Tetris::Tetris(int width, int height)
 	Tetris::playfield = \
 		std::vector<std::vector<int>>(Tetris::TETRIS_PLAYFIELD_WIDTH,
 				std::vector<int>(Tetris::TETRIS_PLAYFIELD_HEIGHT, 0));
-	Tetris::Tetrominos[Tetris::I] = \
-		Tetris::Tetromino {
-			std::tuple<int, int, int>{
-				173,
-				216,
-				230
-			},
-			std::array<std::pair<int, int>, 4>{
-				std::pair<int, int>{3, 1},
-				std::pair<int, int>{4, 1},
-				std::pair<int, int>{5, 1},
-				std::pair<int, int>{6, 1},
-			}
-		};
-	Tetris::Tetrominos[Tetris::L] = \
-		Tetris::Tetromino {
-			std::tuple<int, int, int>{
-				0,
-				0,
-				139
-			},
-			std::array<std::pair<int, int>, 4>{
-				std::pair<int, int>{2, 0},
-				std::pair<int, int>{3, 0},
-				std::pair<int, int>{4, 0},
-				std::pair<int, int>{4, 1},
-			}
-		};
-	Tetris::Tetrominos[Tetris::J] = \
-		Tetris::Tetromino {
-			std::tuple<int, int, int>{
-				240,
-				165,
-				0
-			},
-			std::array<std::pair<int, int>, 4>{
-				std::pair<int, int>{2, 0},
-				std::pair<int, int>{2, 1},
-				std::pair<int, int>{3, 1},
-				std::pair<int, int>{4, 1},
-			}
-		};
-	Tetris::Tetrominos[Tetris::O] = \
-		Tetris::Tetromino {
-			std::tuple<int, int, int>{
-				255,
-				255,
-				0
-			},
-			std::array<std::pair<int, int>, 4>{
-				std::pair<int, int>{4, 0},
-				std::pair<int, int>{5, 0},
-				std::pair<int, int>{4, 1},
-				std::pair<int, int>{5, 1},
-			}
-		};
-	Tetris::Tetrominos[Tetris::T] = \
-		Tetris::Tetromino {
-			std::tuple<int, int, int>{
-				128,
-				0,
-				128,
-			},
-			std::array<std::pair<int, int>, 4>{
-				std::pair<int, int>{2, 1},
-				std::pair<int, int>{3, 0},
-				std::pair<int, int>{3, 1},
-				std::pair<int, int>{4, 1},
-			}
-		};
-	Tetris::Tetrominos[Tetris::S] = \
-		Tetris::Tetromino {
-			std::tuple<int, int, int>{
-				0,
-				255,
-				0
-			},
-			std::array<std::pair<int, int>, 4>{
-				std::pair<int, int>{2, 1},
-				std::pair<int, int>{3, 0},
-				std::pair<int, int>{3, 1},
-				std::pair<int, int>{4, 0},
-			}
-		};
-	Tetris::Tetrominos[Tetris::Z] = \
-		Tetris::Tetromino {
-			std::tuple<int, int, int>{
-				255,
-				0,
-				0
-			},
-			std::array<std::pair<int, int>, 4>{
-				std::pair<int, int>{2, 0},
-				std::pair<int, int>{3, 0},
-				std::pair<int, int>{3, 1},
-				std::pair<int, int>{4, 1},
-			}
-		};
+	Tetris::Tetrominos[Tetris::I] = Tetris::Tetromino {
+		std::tuple<int, int, int>{
+			173,
+			216,
+			230
+		},
+		std::array<std::pair<int, int>, 4>{
+			std::pair<int, int>{3, 1},
+			std::pair<int, int>{4, 1},
+			std::pair<int, int>{5, 1},
+			std::pair<int, int>{6, 1},
+		}
+	};
+	Tetris::Tetrominos[Tetris::L] = Tetris::Tetromino {
+		std::tuple<int, int, int>{
+			0,
+			0,
+			139
+		},
+		std::array<std::pair<int, int>, 4>{
+			std::pair<int, int>{2, 0},
+			std::pair<int, int>{3, 0},
+			std::pair<int, int>{4, 0},
+			std::pair<int, int>{4, 1},
+		}
+	};
+	Tetris::Tetrominos[Tetris::J] = Tetris::Tetromino {
+		std::tuple<int, int, int>{
+			240,
+			165,
+			0
+		},
+		std::array<std::pair<int, int>, 4>{
+			std::pair<int, int>{2, 0},
+			std::pair<int, int>{2, 1},
+			std::pair<int, int>{3, 1},
+			std::pair<int, int>{4, 1},
+		}
+	};
+	Tetris::Tetrominos[Tetris::O] = Tetris::Tetromino {
+		std::tuple<int, int, int>{
+			255,
+			255,
+			0
+		},
+		std::array<std::pair<int, int>, 4>{
+			std::pair<int, int>{4, 0},
+			std::pair<int, int>{5, 0},
+			std::pair<int, int>{4, 1},
+			std::pair<int, int>{5, 1},
+		}
+	};
+	Tetris::Tetrominos[Tetris::T] = Tetris::Tetromino {
+		std::tuple<int, int, int>{
+			128,
+			0,
+			128,
+		},
+		std::array<std::pair<int, int>, 4>{
+			std::pair<int, int>{2, 1},
+			std::pair<int, int>{3, 0},
+			std::pair<int, int>{3, 1},
+			std::pair<int, int>{4, 1},
+		}
+	};
+	Tetris::Tetrominos[Tetris::S] = Tetris::Tetromino {
+		std::tuple<int, int, int>{
+			0,
+			255,
+			0
+		},
+		std::array<std::pair<int, int>, 4>{
+			std::pair<int, int>{2, 1},
+			std::pair<int, int>{3, 0},
+			std::pair<int, int>{3, 1},
+			std::pair<int, int>{4, 0},
+		}
+	};
+	Tetris::Tetrominos[Tetris::Z] = Tetris::Tetromino {
+		std::tuple<int, int, int>{
+			255,
+			0,
+			0
+		},
+		std::array<std::pair<int, int>, 4>{
+			std::pair<int, int>{2, 0},
+			std::pair<int, int>{3, 0},
+			std::pair<int, int>{3, 1},
+			std::pair<int, int>{4, 1},
+		}
+	};
+	Tetris::LevelSpeed = std::array<double, 15>{
+		1.0,
+		0.79300,
+		0.6178,
+		0.47273,
+		0.35520,
+		0.26200,
+		0.18968,
+		0.13473,
+		0.09388,
+		0.06415,
+		0.04298,
+		0.02822,
+		0.01815,
+		0.01144,
+		0.00706
+	};
 	int result = SDL_Init(SDL_INIT_VIDEO);
 	if (result < 0) {
 		throw std::runtime_error(SDL_GetError());
@@ -120,6 +130,8 @@ Tetris::Tetris(int width, int height)
 		height,
 		0
 	);
+	Tetris::renderer = SDL_CreateRenderer(window, -1, 0);
+	// TODO: randomize currentTetromino
 	Tetris::currentTetromino = Tetris::Tetrominos[Tetris::I];
 	Tetris::xleftborder = \
 		(width - Tetris::blocksize * Tetris::TETRIS_PLAYFIELD_WIDTH) / 2;
@@ -131,8 +143,9 @@ Tetris::Tetris(int width, int height)
 void Tetris::loop()
 {
 	SDL_Event e;
+	std::chrono::time_point<std::chrono::system_clock> prev = std::chrono::system_clock::now();
 	bool quit = false;
-	while (!quit) {
+	while (!quit || Tetris::level > Tetris::MAX_LEVELS) {
 		while (SDL_PollEvent(&e)) {
 			switch (e.type) {
 				case SDL_KEYDOWN:
@@ -168,20 +181,34 @@ void Tetris::loop()
 					break;
 			}
 		}
-		if (Tetris::left) {
+		std::chrono::time_point<std::chrono::system_clock> curr = std::chrono::system_clock::now();
+		bool right_time = std::chrono::duration<double>(curr - prev).count() >= Tetris::LevelSpeed.at(Tetris::level - 1);
+		if (Tetris::up) {
+			// TODO: rotate the currentTetromino. consider wall kicks as well
+		}
+		if (Tetris::left && right_time) {
+			for (int i = 0; i < Tetris::TETROMINO_SIZE; ++i) {
+				if (Tetris::currentTetromino.second.at(i).first > Tetris::xleftborder)
+					--Tetris::currentTetromino.second.at(i).first;
+			}
+		}
+		if (Tetris::right && right_time) {
+			for (int i = 0; i < Tetris::TETROMINO_SIZE; ++i) {
+				if (Tetris::currentTetromino.second.at(i).first < Tetris::xrightborder)
+					++Tetris::currentTetromino.second.at(i).first;
+			}
+		}
+		if (Tetris::down && right_time) {
+		}
+		if (Tetris::space) {
 
 		}
-		if (Tetris::right) {
-
-		}
-		if (Tetris::down) {
+		if (right_time) {
+			prev = curr;
 			for (int i = 0; i < Tetris::TETROMINO_SIZE; ++i) {
 				if (Tetris::currentTetromino.second.at(i).second * Tetris::blocksize < Tetris::TETRIS_PLAYFIELD_HEIGHT * Tetris::blocksize - Tetris::blocksize)
 					++Tetris::currentTetromino.second.at(i).second;
 			}
-		}
-		if (Tetris::space) {
-
 		}
 		Tetris::draw();
 	}
@@ -189,34 +216,33 @@ void Tetris::loop()
 
 void Tetris::draw()
 {
-	SDL_Surface *s = SDL_GetWindowSurface(Tetris::window);
-	SDL_FillRect(s, NULL, SDL_MapRGB(s->format, 0, 0, 0));
+	SDL_SetRenderDrawColor(Tetris::renderer, 0, 0, 0, 255);
+	SDL_RenderClear(Tetris::renderer);
+	SDL_SetRenderDrawColor(Tetris::renderer, 100, 100, 100, 255);
 	SDL_Rect playfield_area{
 		Tetris::xleftborder,
 		0,
 		Tetris::blocksize * Tetris::TETRIS_PLAYFIELD_WIDTH,
 		Tetris::blocksize * Tetris::TETRIS_PLAYFIELD_HEIGHT
 	};
-	SDL_FillRect(s, &playfield_area, SDL_MapRGB(s->format, 100, 100, 100));
+	SDL_RenderFillRect(Tetris::renderer, &playfield_area);
 	for (int i = 0; i < Tetris::TETROMINO_SIZE; ++i) {
+		SDL_SetRenderDrawColor(
+			Tetris::renderer,
+			std::get<0>(Tetris::currentTetromino.first),
+			std::get<1>(Tetris::currentTetromino.first),
+			std::get<2>(Tetris::currentTetromino.first),
+			255
+		);
 		SDL_Rect temp{
 			Tetris::xleftborder + Tetris::blocksize * Tetris::currentTetromino.second.at(i).first,
 			Tetris::blocksize * Tetris::currentTetromino.second.at(i).second,
 			Tetris::blocksize,
 			Tetris::blocksize
 		};
-		SDL_FillRect(
-			s,
-			&temp,
-			SDL_MapRGB(
-				s->format,
-				std::get<0>(Tetris::currentTetromino.first),
-				std::get<1>(Tetris::currentTetromino.first),
-				std::get<2>(Tetris::currentTetromino.first)
-			)
-		);
+		SDL_RenderFillRect(Tetris::renderer, &temp);
 	}
-	SDL_UpdateWindowSurface(Tetris::window);
+	SDL_RenderPresent(Tetris::renderer);
 }
 
 Tetris::~Tetris()
--- Tetris.h
+++ Tetris.h
@@ -18,16 +18,20 @@ class Tetris {
 	};
 	std::vector<std::vector<int>> playfield;
 	SDL_Window *window = nullptr;
+	SDL_Renderer *renderer = nullptr;
 	// I J L O S T Z
 	std::array<Tetromino, 7> Tetrominos;
+	std::array<double, 15> LevelSpeed; 
 	Tetromino currentTetromino;
 	const int blocksize = 25;
 	const int TETRIS_PLAYFIELD_WIDTH = 10;
 	const int TETRIS_PLAYFIELD_HEIGHT = 20;
 	const int TETROMINO_SIZE = 4;
+	const int MAX_LEVELS = 15;
 	int xleftborder;
 	int xrightborder;
-	int gravity = 1000;
+	int level = 1;
+	int lines_cleared = 0;
 	bool left = false;
 	bool right = false;
 	bool down = false;
